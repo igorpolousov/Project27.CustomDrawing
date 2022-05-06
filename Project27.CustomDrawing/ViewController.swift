12b09b26cba4b69f8dc9a1c61ba6904a7a5cd35d
Igor Polousov <igorpolousov@gmail.com>
2022-05-06 15:50:47 +0300

    + drawing emoji + drawing star + drawing TWIN

--- Project27.CustomDrawing/ViewController.swift
+++ Project27.CustomDrawing/ViewController.swift
@@ -27,7 +27,7 @@ class ViewController: UIViewController {
     @IBAction func redrawButton(_ sender: Any) {
         currentDrawType += 1
         
-        if currentDrawType > 5 {
+        if currentDrawType > 8 {
             currentDrawType = 0
         }
         
@@ -44,6 +44,12 @@ class ViewController: UIViewController {
             drawLines()
         case 5:
             drawImagesAndText()
+        case 6:
+            drawSurprisedEmoji()
+        case 7:
+            drawStarEmoji()
+        case 8:
+            drawTwinText()
         default:
             break
         }
@@ -181,5 +187,202 @@ class ViewController: UIViewController {
         imageView.image = image
     }
     
+    // challenge 1
+    func drawSurprisedEmoji() {
+        let imgWidth = 512
+        let imgHeight = 512
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: imgWidth, height: imgHeight))
+        
+        let image = renderer.image { ctx in
+            
+            let faceWidth = imgWidth
+            let faceHeight = imgWidth
+            let faceInsets: CGFloat = 20
+            
+            drawFace(ctx: ctx.cgContext, width: faceWidth, height: faceHeight, insets: faceInsets, startX: 0, startY: 0, fillColor: UIColor.yellow.cgColor, strokeColor: UIColor.orange.cgColor)
+
+            let eyeHorizontalMargin: CGFloat = 130
+            let eyeTopMargin: CGFloat = 150
+            let eyeWidth = 60
+            let eyeHeight = 70
+            
+            let leftEyeStartX = eyeHorizontalMargin
+            let leftEyeStartY = eyeTopMargin
+            drawEye(ctx: ctx.cgContext, width: eyeWidth, height: eyeHeight, startX: leftEyeStartX, startY: leftEyeStartY, color: UIColor.orange.cgColor)
+
+            let rightEyeStartX = CGFloat(imgWidth) - CGFloat(eyeWidth) - eyeHorizontalMargin
+            let rightEyrStartY = eyeTopMargin
+            drawEye(ctx: ctx.cgContext, width: eyeWidth, height: eyeHeight, startX: rightEyeStartX, startY: rightEyrStartY, color: UIColor.orange.cgColor)
+
+            let mouthWidth = 100
+            let mouthHeight = 110
+            let mouthStartx = CGFloat((imgWidth - mouthWidth) / 2)
+            let mouthStarty = CGFloat(300)
+
+            drawMouth(ctx: ctx.cgContext, width: mouthWidth, height: mouthHeight, startX: mouthStartx, startY: mouthStarty, color: UIColor.orange.cgColor)
+        }
+        
+        imageView.image = image
+    }
+   
+    // challenge 1
+    func drawFace(ctx: CGContext, width: Int, height: Int, insets: CGFloat, startX: CGFloat, startY: CGFloat, fillColor: CGColor, strokeColor: CGColor) {
+        let face = CGRect(x: 0, y: 0, width: width, height: height).insetBy(dx: insets, dy: insets)
+        
+        ctx.setFillColor(UIColor.yellow.cgColor)
+        ctx.setStrokeColor(UIColor.orange.cgColor)
+        ctx.setLineWidth(5)
+        
+        ctx.translateBy(x: startX, y: startY)
+        ctx.addEllipse(in: face)
+        ctx.drawPath(using: .fillStroke)
+        ctx.translateBy(x: -startX, y: -startY)
+    }
+    
+    // challenge 1
+    func drawEye(ctx: CGContext, width: Int, height: Int, startX: CGFloat, startY: CGFloat, color: CGColor) {
+        let eye = CGRect(x: 0, y: 0, width: width, height: height)
+
+        ctx.setFillColor(color)
+
+        ctx.translateBy(x: startX, y: startY)
+        ctx.addEllipse(in: eye)
+        ctx.drawPath(using: .fill)
+        ctx.translateBy(x: -startX, y: -startY)
+    }
+    
+    // challenge 1
+    func drawMouth(ctx: CGContext, width: Int, height: Int, startX: CGFloat, startY: CGFloat, color: CGColor) {
+        let mouth = CGRect(x: 0, y: 0, width: width, height: height)
+        
+        ctx.setFillColor(color)
+
+        ctx.translateBy(x: startX, y: startY)
+        ctx.addEllipse(in: mouth)
+        ctx.drawPath(using: .fill)
+        ctx.translateBy(x: -startX, y: -startY)
+
+    }
+    
+    // challenge 1
+    func drawStarEmoji() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        
+        let image = renderer.image { ctx in
+            ctx.cgContext.translateBy(x: 256, y: 270)
+            
+            let innerRadius: CGFloat = 94
+            let outerRadius: CGFloat = 245
+            let starPoints = 5
+            
+            let initialInnerPoint = pointOnCircle(radius: innerRadius, angle: 0)
+            ctx.cgContext.move(to: initialInnerPoint)
+
+            for _ in 1...starPoints {
+                let starPoints = CGFloat(starPoints)
+
+                let outerPoint = pointOnCircle(radius: outerRadius, angle: .pi * 2 / (starPoints * 2))
+                ctx.cgContext.addLine(to: outerPoint)
+                
+                let innerPoint = pointOnCircle(radius: innerRadius, angle: .pi * 2 / starPoints)
+                ctx.cgContext.addLine(to: innerPoint)
+
+                ctx.cgContext.rotate(by: -(.pi * 2 / starPoints))
+            }
+            
+            ctx.cgContext.closePath()
+
+            ctx.cgContext.setStrokeColor(UIColor.orange.cgColor)
+            ctx.cgContext.setLineWidth(10)
+            ctx.cgContext.setFillColor(UIColor.yellow.cgColor)
+            ctx.cgContext.setLineJoin(.round)
+            ctx.cgContext.setLineCap(.round)
+            ctx.cgContext.drawPath(using: .fillStroke)
+        }
+        
+        imageView.image = image
+    }
+    
+    // challenge 1
+    func pointOnCircle(radius: CGFloat, angle: CGFloat) -> CGPoint {
+        return CGPoint(x: radius * sin(angle), y: radius * cos(angle))
+    }
+    
+    // challenge 2
+    func drawTwinText() {
+        let imgWidth = 512
+        let imgHeight = 512
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: imgWidth, height: imgHeight))
+
+        // those 2 parameters alone determine the text size, and can be changed
+        let height = 150
+        let spacing = 40
+
+        // center text vertically
+        let top: Int = (imgHeight - height) / 2
+        let bottom = top + height
+        
+        // width is proportional to height
+        let width: Int = height * 2 / 3
+        
+        // center text horizontally
+        //                          T                 W             I             N
+        var startx = (imgWidth - (width + spacing + width + spacing + spacing + width)) / 2
+        
+        let image = renderer.image { ctx in
+            drawT(ctx: ctx.cgContext, top: top, bottom: bottom, startx: startx, width: width)
+
+            startx += width + spacing
+            drawW(ctx: ctx.cgContext, top: top, bottom: bottom, startx: startx, width: width)
+
+            startx += width + spacing
+            drawI(ctx: ctx.cgContext, top: top, bottom: bottom, startx: startx, width: width)
+
+            startx += spacing
+            drawN(ctx: ctx.cgContext, top: top, bottom: bottom, startx: startx, width: width)
+
+            ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+            ctx.cgContext.setLineWidth(10)
+            ctx.cgContext.setLineJoin(.round)
+            ctx.cgContext.setLineCap(.round)
+            ctx.cgContext.drawPath(using: .stroke)
+        }
+
+        imageView.image = image
+    }
+    // challenge 2
+    func drawT(ctx: CGContext, top: Int, bottom: Int, startx: Int, width: Int) {
+        ctx.move(to: CGPoint(x: startx, y: top))
+        ctx.addLine(to: CGPoint(x: startx + width, y: top))
+        ctx.move(to: CGPoint(x: startx + width/2, y: top))
+        ctx.addLine(to: CGPoint(x: startx + width/2, y: bottom))
+    }
+    
+    // challenge 2
+    func drawW(ctx: CGContext, top: Int, bottom: Int, startx: Int, width: Int) {
+        ctx.move(to: CGPoint(x: startx, y: top))
+        ctx.addLine(to: CGPoint(x: Double(startx) + Double(width) * 0.3, y: Double(bottom)))
+        ctx.addLine(to: CGPoint(x: Double(startx) + Double(width) * 0.5, y: Double((top + bottom) / 2)))
+        ctx.addLine(to: CGPoint(x: Double(startx) + Double(width) * 0.7, y: Double(bottom)))
+        ctx.addLine(to: CGPoint(x: startx + width, y: top))
+    }
+
+    // challenge 2
+    func drawI(ctx: CGContext, top: Int, bottom: Int, startx: Int, width: Int) {
+        ctx.move(to: CGPoint(x: startx, y: top))
+        ctx.addLine(to: CGPoint(x: startx, y: bottom))
+   }
+
+    // challenge 2
+    func drawN(ctx: CGContext, top: Int, bottom: Int, startx: Int, width: Int) {
+        ctx.move(to: CGPoint(x: startx, y: bottom))
+        ctx.addLine(to: CGPoint(x: startx, y: top))
+        ctx.addLine(to: CGPoint(x: startx + width, y: bottom))
+        ctx.addLine(to: CGPoint(x: startx + width, y: top))
+    }
 }
+
+
+
+    
 
